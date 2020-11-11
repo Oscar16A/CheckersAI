2896a5a0c02ffe2428abb5355c089e12669eca65
Oscar16A <oscar16amarillas@gmail.com>
2020-11-11 13:44:22 -0800

    Select Best Move (poor utility)

--- src/StudentAI.cpp
+++ src/StudentAI.cpp
@@ -1,5 +1,6 @@
 #include "StudentAI.h"
 #include <random>
+#include <iostream>
 
 //The following part should be completed by students.
 //The students can modify anything except the class name and exisiting functions and varibles.
@@ -21,25 +22,64 @@ Move StudentAI::GetMove(Move move)
         board.makeMove(move,player == 1?2:1);
     }
 	
-	//
+	vector<vector<Move> > moves = board.getAllPossibleMoves(player);
+	//random AI for fisrt move selection
+	//int i = rand() % (moves.size());
+	//vector<Move> checker_moves = moves[i];
+	//int j = rand() % (checker_moves.size());
+	
+	//choosing a random move to start
+	//Move bestMove = checker_moves[j];
+	Move bestMove;
+	int bestScore = -999;
+	
+	Board boardCopy = board;
+	/*
+	boardCopy.makeMove(checker_moves[j], player);
+	if (player == 1)
+	{
+		bestScore = boardCopy.blackCount - boardCopy.whiteCount;
+	}
+	else
+	{
+		bestScore = boardCopy.whiteCount - boardCopy.blackCount;
+	}*/
 
-    vector<vector<Move> > moves = board.getAllPossibleMoves(player);
-	for (int checkerNum = 0; chekcerNum < moves.size(); checkerNum++) 
+	//check all moves for best move
+	for (int checkerNum = 0; checkerNum < moves.size(); checkerNum++) 
 	{
 		for (int moveNum = 0; moveNum < moves[checkerNum].size(); moveNum++)
 		{
-			//make child of root
+			boardCopy = board;
+			boardCopy.makeMove(moves[checkerNum][moveNum], player);
+			if (player == 1)
+			{
+				int moveScore = boardCopy.blackCount - boardCopy.whiteCount;
+				if (moveScore > bestScore)
+				{
+					bestScore = moveScore;
+					bestMove = moves[checkerNum][moveNum];
+				}
+			}
+			else
+			{
+				int moveScore = boardCopy.whiteCount - boardCopy.blackCount;
+				if (moveScore > bestScore)
+				{
+					bestScore = moveScore;
+					bestMove = moves[checkerNum][moveNum];
+				}
+			}
 		}
 	}
-    
+	
 	//random AI 
-	int i = rand() % (moves.size());
-    vector<Move> checker_moves = moves[i];
-    int j = rand() % (checker_moves.size());
-    Move res = checker_moves[j];
+	//int i = rand() % (moves.size());
+    //vector<Move> checker_moves = moves[i];
+    //int j = rand() % (checker_moves.size());
+    //Move res = checker_moves[j];
     
-	
-	
+	Move res = bestMove;
 	board.makeMove(res,player);
     return res;
 }
